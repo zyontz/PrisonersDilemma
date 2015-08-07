9963759bd3b147ed5fde5f42e30ac9367065dd86
teamJD <jeffrey.merz@gmail.com>
2015-08-07 09:15:52 -0400

    Update player3.py
    fixed

--- player3.py
+++ player3.py
@@ -20,7 +20,8 @@ class Player3:
                 return 'c'
             elif history[-1]=='c' and opponent_history[-1]=='b':
                 return 'b' # betray is they were severely punished last time
+            
             else:
-                return 'c' #otherwise collude
+                return 'b' #otherwise collude
 
    
