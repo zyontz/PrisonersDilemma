99a6588be5c0e2622fd00bd8c404dc185544f086
crabmagic <nathan_stanford@bullis.org>
2015-08-07 09:19:54 -0400

    player name, strategy

--- player4.py
+++ player4.py
@@ -12,12 +12,12 @@ class Player4:
    #Defining functions for each child(instance) of class Car
    def strategy(self,history, opponent_history, score, opponent_score, getting_team_name):
         if getting_team_name:
-            return 'betray every 3rd round'
+            return 'reciprocator'
         else:
             # use history, opponent_history, score, opponent_score
             # to compute your strategy
             size = len(history)
-            if(size%3==0): #the number of rounds played is a multiple of 3
+            if(history[-1]=='c'): #the number of rounds played is a multiple of 3
                 return 'c'
             else:
                 return 'b'
