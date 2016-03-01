c187adf9025ab55f1275a08ec32bb0fa51dc7ad4
Dave Gillis <davegillis4@Gmail.com>
2016-02-29 20:50:41 -0500

    Update player11.py

--- player11.py
+++ player11.py
@@ -18,7 +18,7 @@ class Player11:
             # to compute your strategy
             if len(opponent_history)==0: #It's the first round: collude
                 return 'c'
-            elif history[-1]=='c' and opponent_history[-1]=='b':
+            elif history[-1]=='b' and opponent_history[-1]=='b':
                 return 'b' # betray is they were severely punished last time
             else:
                 return 'c' #otherwise collude
